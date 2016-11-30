ba9c6e9a6830f538affde5dc163a5258a4bf0055
UdeeshaShehan <udeesha.12@cse.mrt.ac.lk>
2016-11-30 10:54:40 +0530

    safwan parts

--- app/src/main/java/com/example/usid/mpos/UI/ReportFragment.java
+++ app/src/main/java/com/example/usid/mpos/UI/ReportFragment.java
@@ -42,7 +42,6 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.RunnableFuture;
 
 /**
  * UI for showing sale's record.
