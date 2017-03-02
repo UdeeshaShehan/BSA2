9b164e03d5310561e397183a1bf327d9e86c58aa
Mursith <nmursith@gmail.com>
2017-03-02 22:21:19 +0530

    notification addded

--- app/src/main/java/com/example/usid/mpos/UI/ReportFragment.java
+++ app/src/main/java/com/example/usid/mpos/UI/ReportFragment.java
@@ -11,6 +11,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
@@ -407,6 +410,7 @@ public class ReportFragment extends UpdatableFragment implements PriceCommunicat
 
 						  expiryDate.setText(eMonth + "/" + eYear);
 						  cardNo.setText(cardNu);
+						  playDefaultNotificationSound();
 					  }
 				  }
 			  }catch (Exception e){
@@ -420,6 +424,11 @@ public class ReportFragment extends UpdatableFragment implements PriceCommunicat
 		return view;
 	}
 
+	private void playDefaultNotificationSound() {
+		Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+		Ringtone r = RingtoneManager.getRingtone(getActivity().getApplicationContext(), notification);
+		r.play();
+	}
 	private void connectMQTT(final MQTTConnection mqttConnection){
 
 		AsyncTask asyncTask = new AsyncTask() {
